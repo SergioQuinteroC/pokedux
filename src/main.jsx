8412adaf5910341cdf601b532e0e6fe7ae543d1b
SergioQuinteroC <sergio.quinteroc@udea.edu.co>
2023-01-03 18:54:33 -0500

    add combine reducers

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { pokemonsReducer } from "./reducers/pokemons";
 import { Provider } from "react-redux";
 import {
   applyMiddleware,
@@ -11,6 +10,7 @@ import {
 import thunk from "redux-thunk";
 import { featuring, logger, nameUpperCase } from "./middlewares";
 import "./index.css";
+import rootReducer from "./reducers/rootReducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -27,7 +27,7 @@ const composeAlt =
 
 const composedEnhancers = composeAlt(applyMiddleware(thunk, logger, nameUpperCase));
 
-const store = createStore(pokemonsReducer, composedEnhancers);
+const store = createStore(rootReducer, composedEnhancers);
 
 root.render(
   <React.StrictMode>
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,9 +1,8 @@
 import { fromJS } from "immutable";
-import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types";
+import { SET_FAVORITE, SET_POKEMONS } from "../actions/types";
 
 const initialState = fromJS({
   pokemons: [],
-  loading: false,
 });
 
 export const pokemonsReducer = (state = initialState, action) => {
@@ -30,10 +29,6 @@ export const pokemonsReducer = (state = initialState, action) => {
         ["pokemons", currentPokemonIndex, "favorite"],
         !isFavorite
       );
-
-    case SET_LOADING:
-      //  return { ...state, loading: action.payload };
-      return state.setIn(["loading"], action.payload);
 
     default:
       return state;
