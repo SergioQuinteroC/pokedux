b514294d665315a0a77e49efca1c1029759d7546
SergioQuinteroC <sergio.quinteroc@udea.edu.co>
2023-01-03 13:41:40 -0500

    ADD redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import { getPokemon } from "./api";
+import { setPokemons as setPokemonsActions } from "./actions";
 import logo from "./statics/logo.svg";
 import "./App.css";
 
-function App() {
-  const [pokemons, setPokemons] = useState([]);
+function App({ pokemons, setPokemons }) {
+  /*   const [pokemons, setPokemons] = useState([]); */
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -31,4 +33,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
